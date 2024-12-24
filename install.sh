80353be5f16e3f95f05e48d7a2198a07c10eaedb
maxraulea <max.raulea@gmail.com>
2024-12-24 23:20:05 +0100

    new files

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-doas pkg install wayland hyprland hyprpicker hyprpaper xdg-desktop-portal-hyprland drm-510-kmod neofetch kitty wlogout hypridle hyprlock evince vim firefox 
-
+doas pkg install wayland hyprland hyprpicker hyprpaper xdg-desktop-portal-hyprland drm-510-kmod neofetch kitty wlogout hypridle hyprlock evince vim firefox dunst xf86-input-libinput swww grim
+doas cp -r /dots ~/.config/
